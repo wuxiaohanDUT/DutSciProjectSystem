db3e592f4bc4dae09b74dd56f3eccc8c73aa4c18
wuxiaohanDUT <2658343834@qq.com>
2021-12-24 14:09:31 +0800

    Initial commit

---
+++ src/main/java/com/dut/sci/project/DutSciProjectSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dut.sci.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutSciProjectSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutSciProjectSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dut/sci/project/DutSciProjectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dut.sci.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutSciProjectSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
